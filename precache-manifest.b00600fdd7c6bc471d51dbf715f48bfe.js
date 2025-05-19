65ee6c5b2f800e71602b53355b468cc8d53c4bd2
saish20 <saish20@users.noreply.github.com>
2025-05-19 09:40:09 +0000

    deploy: 53f4c9062d78215fa964c5ddc2e121bce945080c

---
+++ precache-manifest.b00600fdd7c6bc471d51dbf715f48bfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d8548aa4bada82b659c5b029663823",
+    "url": "/ipl-fantasy-app/index.html"
+  },
+  {
+    "revision": "9889e623191eb5ccd6fe",
+    "url": "/ipl-fantasy-app/static/css/main.111f16c2.chunk.css"
+  },
+  {
+    "revision": "3a740e66f0e7d5c7aea5",
+    "url": "/ipl-fantasy-app/static/js/2.04760c7f.chunk.js"
+  },
+  {
+    "revision": "bb7a58daa482d77228a04eaea795fd3f",
+    "url": "/ipl-fantasy-app/static/js/2.04760c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e30309ad3336b0f062",
+    "url": "/ipl-fantasy-app/static/js/3.a5e72306.chunk.js"
+  },
+  {
+    "revision": "9889e623191eb5ccd6fe",
+    "url": "/ipl-fantasy-app/static/js/main.3160704e.chunk.js"
+  },
+  {
+    "revision": "330642ef5c98bd7112d4",
+    "url": "/ipl-fantasy-app/static/js/runtime-main.f2648ae8.js"
+  },
+  {
+    "revision": "54a4de74dd821570496d6a42cdcd9503",
+    "url": "/ipl-fantasy-app/static/media/ipl-logo.54a4de74.png"
+  }
+]);
